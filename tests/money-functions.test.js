8eb50fbe573dd166cb01e881d1ad11bf17c771ef
Desiree <dezlaleman@gmail.com>
2023-08-07 10:31:18 -0400

    finished lab

--- tests/money-functions.test.js
+++ tests/money-functions.test.js
@@ -1,7 +1,24 @@
+const { formatCurrency, getCoins } = require("../src/js/money-functions");
+
 describe("formatCurrency", () => {
-  test.todo("add formatCurrency tests here");
+  test(`Given the amount 0, it returns "$0.00"`, () => {
+    expect(formatCurrency(0)).toBe("$0.00");
+  });
+  test(`Given the amount 527.6789, it returns "$527.68"`, () => {
+    expect(formatCurrency(527.6789)).toBe("$527.68");
+  });
+  test(`Given the amount 0, it returns "$0.00"`, () => {
+    expect(formatCurrency(-1)).toBe("-$1.00");
+  });
 });
 
 describe("getCoins", () => {
-  test.todo("add getCoins tests here");
+  test("32 cents produces: quarters: 1, dimes: 0, nickels: 1, pennies: 2.", () => {
+    expect(getCoins(32)).toEqual({
+      quarters: 1,
+      dimes: 0,
+      nickels: 1,
+      pennies: 2,
+    });
+  });
 });
